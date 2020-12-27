0f50bd334777e7bd0e0021e66123c0e14812a08f
Yan Shatskiy <writemetolinkedin@git.io>
2020-12-27 12:55:51 +0100

    All tasks of module 3 have been solved

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,17 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
+  const sortedList = str
+    .split(",")
+    .join(" ")
+    .split(" ")
+    .reduce((numbers, elem) => {
+      if (elem !== "" && !Number.isNaN(+elem)) {
+        numbers.push(+elem);
+      }
+      return numbers;
+    }, [])
+    .sort((a, b) => a - b);
+
+  return { min: sortedList[0], max: sortedList[sortedList.length - 1] };
 }
